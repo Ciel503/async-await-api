bcbe01983b5992d073844b653bdffb340943ad04
Franciel <francielmsq@gmail.com>
2025-12-25 11:55:42 -0300

    feat: estrutura inicial

---
+++ script.js
@@ -0,0 +1,36 @@
+var botao = document.getElementById("carregar");
+var resultado = document.getElementById("resultado");
+
+const carregar = async () => {
+    botao.textContent = "Carregando...";
+    botao.disabled = true;
+    resultado.innerHTML = "";
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+
+        if (!response.ok) {
+            throw new Error("Erro na requisição");
+        }
+
+        const data = await response.json();
+
+        data.forEach(user => {
+            resultado.innerHTML += `
+    <p>
+                    Nome: ${user.name}<br>
+                    Email: ${user.email}<br>
+                    Cidade: ${user.address.city}
+                </p>
+                <hr>
+            `;
+        });
+
+    } catch (error) {
+        resultado.innerHTML = "Erro ao carregar usuários";
+    } finally {
+        botao.textContent = "CARREGAR";
+        botao.disabled = false;
+    }
+}
+
+botao.addEventListener('click', carregar);
